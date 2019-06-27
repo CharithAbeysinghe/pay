83741791fc829b1b747ad709928aaf06daad4144
klcp <cpclans@gmail.com>
2019-06-27 23:22:31 +0530

    adding files

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Capp from './component/Capp'
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        
+         <Capp/>
+       
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Capp.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+// import { deflate } from 'zlib';
+
+
+// const Capp: React.FC = () =>{
+
+    class Capp extends React.Component{
+        public render(){
+            return(
+<div>charith</div>
+
+            );
+        }
+    }
+
+// return();
+
+
+// }
+
+export default Capp;
